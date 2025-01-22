5a6ba4b8e557d0cce36798e553e13b93b96199c0
tarinagarwal <theultimatemania64@gmail.com>
2025-01-22 20:24:22 +0530

    setup webhooks for auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { SignOutButton, SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <br />
+      <SignUpButton>Signup</SignUpButton>
+      <br />
+      <SignOutButton>Signout</SignOutButton>
+    </div>
+  );
 }
